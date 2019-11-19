ccf8e085f2b4503fdfe71aa4058976965ed55640
Gabriela C. dos Santos <gabrielacsantos.engunb@gmail.com>
2019-11-18 21:13:39 -0300

    Update Luximetro.ino

--- Luximetro.ino
+++ Luximetro.ino
@@ -1,24 +1,40 @@
-   
-int LED = 10; //Pino ligado ao anodo do led  
-int LDR = 5;  //Pino ligado ao LDR  
-int valor_LDR = 0; //Armazena valor lido do LDR, entre 0 e 1023  
-double luminosidade = 0; //Valor de luminosidade do led     
+#define LDR_PIN                   0
+#define MAX_ADC_READING           1023
+#define ADC_REF_VOLTAGE           5.0
+#define REF_RESISTANCE            5030  
+#define LUX_CALC_SCALAR           12518931
+#define LUX_CALC_EXPONENT         -1.405
 
-void setup()  
-{    
-  Serial.begin(9600);    //Inicializa a serial    
-  pinMode(LED, OUTPUT); //Define o pino do led como saída    
-  pinMode(LDR, INPUT);  //Define o pino do LDR como entrada 
-   
-}     
-  void loop()  
- {    
-  valor_LDR = analogRead(LDR);  //Le o valor - analogico - do LDR  
-  luminosidade = map(valor_LDR, 0, 255, 0, 1023); // Mapeia os valores de resistencia em relação a luminosidade   
-  luminosidade = 255.84 * pow (valor_LDR, -10/9); // Equação da luminosidade
-  Serial.print("Valor lido do LDR : ");     //Mostra o valor lido do LDR no monitor serial    
-  Serial.print(valor_LDR);    
-  Serial.print(" = Luminosidade : ");    //Mostra o valor da luminosidade no monitor serial    
-  Serial.println(luminosidade);     //Acende o led com luminosidade variável  
-  analogWrite(LED, luminosidade);   // PWM do LED, mas será substituido pelo relê
+void setup(void) 
+{
+  Serial.begin(9600);
+  //Serial.println(F("Luxímetro")); Serial.println("");
+}
+
+
+void loop(void) 
+{  
+  
+  int   ldrRawData;
+  float resistorVoltage, ldrVoltage;
+  float ldrResistance;
+  float ldrLux;
+  
+    
+  ldrRawData = analogRead(LDR_PIN);
+
+
+  resistorVoltage = (float)ldrRawData / MAX_ADC_READING * ADC_REF_VOLTAGE;
+
+ 
+  ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage; 
+  ldrResistance = ldrVoltage/resistorVoltage * REF_RESISTANCE;
+  ldrLux = LUX_CALC_SCALAR * pow(ldrResistance, LUX_CALC_EXPONENT);
+
+ 
+ Serial.print(ldrLux); 
+
+
+  
+  delay(1000);
 }
